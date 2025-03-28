8103f775cafd5d257ec29a58a6a26dd25fa0f51e
aleks.karczewski <aleks.karczewski@student.put.poznan.pl>
2025-03-28 19:00:54 +0100

    Add project files.

---
+++ WIn11_Info/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WIn11_Info
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowSN_Click(object sender, RoutedEventArgs e)
+        {
+            String sn=Tools.GetLocalSN();
+            if(sn != "1")
+            {
+                btnShowSN.Content = "SN: " + sn;
+            }
+        }
+
+        private void btnShowIP_Click(object sender, RoutedEventArgs e)
+        {
+            String ip=Tools.GetLocalIPAddress();
+            btnShowIP.Content = ip;
+        }
+
+        private void btnShowCPU_Click(object sender, RoutedEventArgs e)
+        {
+            String cpu=Tools.getCPU();
+            txtBlockCpu.Text = cpu;
+            //btnShowCPU.Content = Tools.getCPU();
+        }
+
+        private void btnShowHostName_Click(object sender, RoutedEventArgs e)
+        {
+            btnShowHostName.Content=System.Net.Dns.GetHostName();
+        }
+
+        private void btnShowLAN_Click(object sender, RoutedEventArgs e)
+        {
+            String lan = Tools.GetLocalMac_Lan();
+            if(lan!="")
+            {
+                btnShowLAN.Content = lan;
+            }
+            else
+            {
+                MessageBox.Show("Error reading MAC address");
+            }
+        }
+
+        private void btnShowWLAN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String mac=Tools.GetLocalMac_Wlan2();
+                if(mac!="0")
+                {
+                    btnShowWLAN.Content = mac;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnDhcpRecord_Click(object sender, RoutedEventArgs e)
+        {
+            btnShowLAN_Click(sender,e);
+            btnShowWLAN_Click(sender, e);
+            lblDhcpRecord1.Content="host "+ System.Net.Dns.GetHostName().ToString()+"__LAN { hardware ethernet "+
+                btnShowLAN.Content+"; fixed-address "+txtBoxDhcpRecord.Text.ToString() + ";}\n";
+
+            lblDhcpRecord2.Content = "host " + System.Net.Dns.GetHostName().ToString() + "__LAN { hardware ethernet " +
+                btnShowWLAN.Content + "; fixed-address " + txtBoxDhcpRecord.Text.ToString() + ";}\n";
+        }
+    }
+}
---
+++ WIn11_Info/Tools.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+// Allows to use Win32 Classes
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Windows;
+
+
+
+namespace WIn11_Info
+{
+    internal class Tools
+    {
+        public static String GetLocalIPAddress()
+        {
+            
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
+
+        public static String GetLocalSN()
+        {
+
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("SELECT SerialNumber, SMBIOSBIOSVersion, ReleaseDate FROM Win32_BIOS");
+
+            ManagementObjectCollection information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                {
+                    if (data.Name.ToString().StartsWith("SerialN"))
+                    {
+                        searcher.Dispose();
+                        return data.Value.ToString();
+                    }
+                }
+            }
+            searcher.Dispose();
+            return "1";
+        }
+
+        public static String getCPU()
+        {
+            String cpu = "";
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("SELECT Name from Win32_Processor");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                cpu += obj["Name"];
+            }
+
+            return cpu;
+        }
+
+        public static String GetLocalMac_Lan()
+        {
+            String addr = "";
+            foreach (NetworkInterface n in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (n.Name.StartsWith("Ethernet"))
+                {
+                    addr = n.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            String lan = "";
+            for (int i = 0; i < addr.Length; i++)
+            {
+                if (i % 2 == 0 && i > 0)
+                {
+                    lan += ":";
+                }
+                lan += addr[i];
+
+            }
+            return lan;
+        }
+
+        public static string GetLocalMac_Wlan2()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter");
+            ManagementObjectCollection adapters = searcher.Get();
+
+            foreach (ManagementObject adapter in adapters)
+            {
+                /*
+                MessageBox.Show("Nazwa: " + adapter["Name"] + " Typ interfejsu: " + adapter["AdapterType"]
+                    + " Status: " + adapter["NetConnectionStatus"] + " MAC: " + adapter["MACAddress"]);
+                */
+                if (adapter["Name"].ToString().Contains("Wi-Fi") &&
+                    (adapter["AdapterType"].ToString().Contains("802.3")) || adapter["AdapterType"].ToString().Contains("802.11"))
+                {
+                    return adapter["MACAddress"].ToString();
+                }
+            }
+            return "0";
+        }
+    }
+}
